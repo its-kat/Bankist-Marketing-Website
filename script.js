c24d7b328da6babe2a3109ed0cacf2c9342cce99
its-kat <khalsell83@gmail.com>
2023-03-20 19:22:49 -0700

    practice code for styles, attributes, and classes.

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ document.addEventListener('keydown', function (e) {
 });
 /////////////////////////////////
 
-// Selecting elements
+////////////// Selecting elements
 console.log(document.documentElement);
 console.log(document.head);
 console.log(document.body);
@@ -46,7 +46,7 @@ console.log(allButtons);
 
 console.log(document.getElementsByClassName('btn'));
 
-// Creating and inserting elements
+////////////// Creating and inserting elements
 
 const message = document.createElement('div');
 message.classList.add('cookie-message');
@@ -62,11 +62,63 @@ header.append(message);
 // header.before(message);
 // header.after(message);
 
-// Delete elements
-
+////////////// Delete elements
 document
   .querySelector('.btn--close-cookie')
   .addEventListener('click', function () {
     message.remove();
     // message.parentElement.replaceChild(message);
   });
+
+////////////// Styles
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+console.log(message.style.color);
+console.log(message.style.backgroundColor);
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+////////////// Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.className);
+
+logo.alt = 'Beautiful minimalist logo';
+
+//Non-standard
+console.log(logo.designer);
+console.log(logo.getAttribute('designer'));
+logo.setAttribute('company', 'Bankist');
+
+console.log(logo.src);
+
+//get relative position
+console.log(logo.getAttribute('src'));
+
+const link = document.querySelector('.nav__link--btn');
+
+// get absolute position
+console.log(link.href);
+
+//returns the URL as I wrote it in the HTML.
+console.log(link.getAttribute('href'));
+
+//////////////  Data attributes
+console.log(logo.dataset.versionNumber);
+
+////////////// Classes
+logo.classList.add('c', 'j');
+logo.classList.remove('c', 'j');
+logo.classList.toggle('c');
+logo.classList.contains('c'); // not includes
+
+// DON'T USE - this will override all the existing classes and also it allows us to only put one class on any element, all right, so again, only one class and it will override whatever is already there.
+
+logo.className = 'jonas';
