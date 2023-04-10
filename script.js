3ab05e3fda9efa962240356c24e13aa28ba25b5d
its-kat <khalsell83@gmail.com>
2023-04-10 15:46:50 -0700

    added nav event listner to return opacity with animation

--- script.js
+++ script.js
@@ -150,7 +150,21 @@ nav.addEventListener('mouseover', function (e) {
   }
 });
 
-nav.addEventListener('mouseout', function (e) {});
+nav.addEventListener('mouseout', function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+
+    const logo = link.closest('.nav').querySelector('img');
+
+    // console.log(siblings, logo);
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = 1;
+    });
+    logo.style.opacity = 1;
+  }
+});
 
 /////////////////////////////////
 /////////////////////////////////
