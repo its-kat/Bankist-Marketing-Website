c007c9671c56c0ad557853b9c1732922544e29f6
its-kat <khalsell83@gmail.com>
2023-04-10 15:44:27 -0700

    added nav event listner for opacity animation

--- script.js
+++ script.js
@@ -7,6 +7,14 @@ const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
 
+const nav = document.querySelector('.nav');
+
+const tabs = document.querySelectorAll('.operations__tab');
+
+const tabsContainer = document.querySelector('.operations__tab-container');
+
+const tabsContent = document.querySelectorAll('.operations__content');
+
 ///////////////////////////////////////
 // Modal window
 
@@ -96,12 +104,8 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
   }
 });
 
-//////////////// Tabbed Component
-const tabs = document.querySelectorAll('.operations__tab');
-
-const tabsContainer = document.querySelector('.operations__tab-container');
-
-const tabsContent = document.querySelectorAll('.operations__content');
+/////////////////////////////////
+// Tabbed Component
 
 //event delegation
 tabsContainer.addEventListener('click', function (e) {
@@ -124,14 +128,36 @@ tabsContainer.addEventListener('click', function (e) {
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add(`operations__content--active`);
 });
-
 // tabs.forEach(t => t.addEventListener('click', () => console.log('TAB')));
 
 /////////////////////////////////
+// Menu fade animation
+
+//mouseenter doesn't bubble up
+nav.addEventListener('mouseover', function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+
+    const logo = link.closest('.nav').querySelector('img');
+
+    // console.log(siblings, logo);
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = 0.5;
+    });
+    logo.style.opacity = 0.5;
+  }
+});
+
+nav.addEventListener('mouseout', function (e) {});
+
+/////////////////////////////////
 /////////////////////////////////
 /////////////////////////////////
 
-////////////// Selecting, Creating, and Deleting celements
+/////////////////////////////////
+//Selecting, Creating, and Deleting celements
 // console.log(document.documentElement);
 // console.log(document.head);
 // console.log(document.body);
@@ -272,7 +298,8 @@ tabsContainer.addEventListener('click', function (e) {
 //   false
 // );
 
-////////////// DOM Traversing
+/////////////////////////////////
+// DOM Traversing
 // const h1 = document.querySelector('h1');
 
 // // Going downwards: child
