a4b97f342c4bfa8ba8926031e6fde68722975039
its-kat <khalsell83@gmail.com>
2023-04-13 07:05:28 -0700

    added slider component

--- script.js
+++ script.js
@@ -267,25 +267,39 @@ const btnRight = document.querySelector('.slider__btn--right');
 let curSlide = 0;
 const maxSlide = slides.length;
 
-const slider = document.querySelector('.slider');
-slider.style.transform = 'scale(0.4) translateX(-800px)';
-slider.style.overflow = 'visible';
+// const slider = document.querySelector('.slider');
+// slider.style.transform = 'scale(0.4) translateX(-800px)';
+// slider.style.overflow = 'visible';
 
-slides.forEach((s, i) => (s.style.transform = `translateX(${100 * i}%)`));
-// 0%, 100%, 200%, 300%
+const goToSlide = function (slide) {
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX( ${100 * (i - slide)}% )`)
+  );
+};
+goToSlide(0);
 
 // Next slide
-btnRight.addEventListener('click', function () {
+const nextSlide = function () {
   if (curSlide === maxSlide - 1) {
     curSlide = 0;
   } else {
     curSlide++;
   }
+  goToSlide(curSlide);
+};
 
-  slides.forEach(
-    (s, i) => (s.style.transform = `translateX( ${100 * (i - curSlide)}% )`)
-  );
-});
+const prevSlide = function () {
+  if (curSlide === 0) {
+    curSlide = maxSlide - 1;
+  } else {
+    curSlide--;
+  }
+
+  goToSlide(curSlide);
+};
+
+btnRight.addEventListener('click', nextSlide);
+btnLeft.addEventListener('click', prevSlide);
 // curSlide = 1, 0%, 100%, 200%
 
 /////////////////////////////////
